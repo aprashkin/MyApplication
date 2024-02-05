b04393f692c0be5046526be321da18f8e3af725d
aprashkin <dandrianov02@gmail.com>
2024-02-05 12:22:28 +0300

    working

--- app/src/main/java/com/example/myapplication/Login_Page.java
+++ app/src/main/java/com/example/myapplication/Login_Page.java
@@ -2,7 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Login_Page extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class Login_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        Button sign_up = findViewById(R.id.sign_up);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), Sign_Up.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
